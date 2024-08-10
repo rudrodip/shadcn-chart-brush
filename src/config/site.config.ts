60091741ab43a4195533d4253e53c477d16815d4
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-11 01:30:28 +0600

    init

---
+++ src/config/site.config.ts
@@ -0,0 +1,26 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Zoomable Chart with Brush",
+  title: "Zoomable Chart with Brush - ShadCN & Recharts",
+  description:
+    "A simple example of a zoomable chart with brush using ShadCN and Recharts.",
+  keywords: [
+    "shadcn",
+    "chart",
+    "brush",
+    "zoom",
+    "typescript",
+    "react",
+  ],
+  creator: {
+    name: "rds_agi",
+    url: "https://rdsx.dev",
+  },
+  siteUrl: "https://shadcn-chart-brush.rdsx.dev",
+  ogImage: "https://shadcn-chart-brush.rdsx.dev/og.png",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/shadcn-chart-brush",
+  },
+};
